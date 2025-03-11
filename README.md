0f3d70a1fc561b5a54962cfaa3f9c77bfca207b1
Deepalakshmi Lakshminarasimhan <writemail.deepa@gmail.com>
2025-03-11 18:34:32 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+#  Yashi's Bakery Bake Me A Cake webpage
+
+This the Yashi's Bakery Bake Me A Cake webpage Webpage Project built Design on HTML , CSS and  the BOOSTRAP framework and function and the validation on javascript.
+ 
+ * Follow the below steps to access the webpage.
+* For run the webpage. First start the server and open the webpage.
+
+* The individual pages are index.html, about.html, contactus.html, orderitem, registration form , sing-in  and to view the images.
+
+1. index.html - It is the Yashi's Bakery Bake Me A Cake home page. The modules made with a lot of tags and it includes Navigation,  logo to display the image to like in all the webpages.  weclome to our webpage and Hero image adding,sections, about us this website. This section carousel use for the displaying slide show images. To Navigation to all webpage in this index page.
+
+2. about.html - It contains are the information about the Yashi's Bakery Bake Me A Cake-webpage. 
+
+3. order-item.html - It Contains are in this section open the Card to diplay the card infromation for cake, icecream, cupcake to select and order to place the card this this website. This section open the container to diplay the product details infromation like us category, size and price to display the table format using the bootstrap classes.
+
+4. contact-us.html - It contains are the get the Contact details and know the Yashi's Bakery Bake Me A Cake-webpage. It contains are the contact information To find the Yashi's Bakery Bake Me A Cake-webpage location so using this Google map. To find the User-Information-webpage location so using this Google map . This is the User-Information-webpage address, contact and  website address information.
+
+5. sign-in.html - It contains the webpage login the user to type the valid username and password. This button When click the button access to javascript function calles to the display the success message.
+
+6. registration.html - It contains the webpage for user fill up the registration form. This section open the container to diplay the registraion form on this website. User Registration Form using the form alignment. This section open the Grid system structuring usig row wise to diplay the registraion form on this website. Each row contains label and input box that's validation form javascript. so That's came automatically validataion in the registraion form on this website. This method list down states for the select component when page gets loaded. 
---
+++ js/script.js
@@ -0,0 +1,107 @@
+// This method list down states for the select component when page gets loaded 
+function loadState() {
+
+  const usStatesArray = [{ value: "None", text: "Select State..." },{ value: "AL", text: "Alabama" },{ value: "AK", text: "Alaska" },{ value: "AZ", text: "Arizona" },{ value: "AR", text: "Arkansas" },{ value: "CA", text: "California" },{ value: "CO", text: "Colorado" },{ value: "CT", text: "Connecticut" },{ value: "DC", text: "District of Columbia" },{ value: "DE", text: "Delaware" },{ value: "FL", text: "Florida" },{ value: "GA", text: "Georgia" },{ value: "HI", text: "Hawaii" },{ value: "ID", text: "Idaho" },{ value: "IL", text: "Illinois" },{ value: "IN", text: "Indiana" },{ value: "IA", text: "Iowa" },{ value: "KS", text: "Kansas" },{ value: "KY", text: "Kentucky" },{ value: "LA", text: "Louisiana" },{ value: "ME", text: "Maine" },{ value: "MD", text: "Maryland" },{ value: "MA", text: "Massachusetts" },{ value: "MI", text: "Michigan" },{ value: "MN", text: "Minnesota" },{ value: "MS", text: "Mississippi" },{ value: "MO", text: "Missouri" },{ value: "MT", text: "Montana" },{ value: "NE", text: "Nebraska" },{ value: "NV", text: "Nevada" },{ value: "NH", text: "New Hampshire" },{ value: "NJ", text: "New Jersey" },{ value: "NM", text: "New Mexico" },{ value: "NY", text: "New York" },{ value: "NC", text: "North Carolina" },{ value: "ND", text: "North Dakota" },{ value: "OH", text: "Ohio" },{ value: "OK", text: "Oklahoma" },{ value: "OR", text: "Oregon" },{ value: "PA", text: "Pennsylvania" },{ value: "RI", text: "Rhode Island" },{ value: "SC", text: "South Carolina" },{ value: "SD", text: "South Dakota" },{ value: "TN", text: "Tennessee" },{ value: "TX", text: "Texas" },{ value: "UT", text: "Utah" },{ value: "VT", text: "Vermont" },{ value: "VA", text: "Virginia" },{ value: "WA", text: "Washington" },{ value: "WV", text: "West Virginia" },{ value: "WI", text: "Wisconsin" },{ value: "WY", text: "Wyoming"}];
+  const selectStateElement = document.getElementById("statesSelect");
+  usStatesArray.forEach(state => {
+    const option = document.createElement("option");
+    option.value = state.value;
+    option.text = state.text;
+    selectStateElement.appendChild(option);
+  });
+}
+
+//User Name Validation
+
+function userNameValidation(userNameValue) {
+  console.log(userNameValue);
+  if (!userNameValue) {
+    console.log("Username cannot be empty.");
+  }
+  const usernameRegex = /^[a-zA-Z][a-zA-Z0-9_]{5,19}$/;
+  console.log(usernameRegex.test(userNameValue))
+  if (!usernameRegex.test(userNameValue)) {
+    console.log("Username must start with a letter, and can only contain letters, numbers, and underscores. It must be 6-20 characters long.");
+    alert("Username must start with a letter, and can only contain letters, numbers, and underscores. It must be 6-20 characters long.");
+  }
+}
+
+//password Validation
+
+function validatePassword(passwordValue) {
+  console.log(passwordValue);
+  if (!passwordValue) {
+    console.log("Password cannot be empty.");
+  }
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-+.]).{6,20}$/;
+  console.log(passwordRegex.test(passwordValue))
+  if (!passwordRegex.test(passwordValue)) {
+    console.log("Wrong Password");
+    alert("Password must contain at least one digit, one lowercase, one uppercase, special character [!@#$%^&*()\-+.] and must be between 6 and 20 characters in length.       'Abcdef123!'");
+  }
+}
+
+//Re-type password
+
+function validateRetypePassword(repasswordValue) {
+  console.log(repasswordValue);
+  if (!repasswordValue) {
+    console.log("Password cannot be empty.");
+  }
+  const repasswordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-+.]).{6,20}$/;
+  console.log(repasswordRegex.test(repasswordValue))
+  if (!repasswordRegex.test(repasswordValue)) {
+    console.log("Wrong Password");
+    alert("Password must contain at least one digit, one lowercase, one uppercase, special character [!@#$%^&*()\-+.] and must be between 6 and 20 characters in length.       'Abcdef123!' ");
+  }
+}
+
+// Date Dispaly of Validation
+
+$('#datepicker').datepicker({
+  uiLibrary: 'bootstrap5',
+  format: 'mm/dd/yyyy',
+  autoclose: true,
+  todayHighlight: true,
+ /* startDate: '01/01/1900',
+  endDate: '12/31/2100',
+  disableDaysOfWeek: [],
+  disableDates: []*/
+});
+
+// String only type in city name
+
+function validateCityName(inputtxt) {
+  const letters = /^[a-zA-Z]+$/;
+  if (!letters.test(inputtxt))
+    alert("Please Type only String characters only");  
+}
+
+
+// Number only type in zip 
+
+function validateZipNumber(inputnum) {
+  const zipCodeRegex = /^[0-9]{5}(?:-[0-9]{4})?$/;
+  if (!zipCodeRegex.test(inputnum))
+    alert("Please input Numeric characters only and it's must be 5 numbers....");
+}
+
+// email id validation
+
+function isValidEmaiId(emailValue) {
+  console.log(emailValue);
+  if (!emailValue) {
+    console.log("Username cannot be empty.");
+  }
+  let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!regex.test(emailValue)) {
+    console.log("Invalid Email address");
+    alert("Invalid Email address");
+  }
+}
+
+// when the click sumbit botton this function called
+function sumbitSignIn() {
+  alert("Sign in Successfully");
+}
+
